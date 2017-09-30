5235cdf4a39da2edfae61204934d31e644dd0f78
1365159558 <1365159558@qq.com>
2017-09-30 10:02:00 +0800

    menu

---
+++ lab2/menu.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{    char cmd;
+    while(1)
+     {
+        scanf(%c,cmd);
+        if (cmd=="help")
+           {printf("help");}
+        else if(cmd=="exit")
+          {printf("exit");}
+        else if(cmd=="A")
+          {printf("a");}
+        else if (cmd=="B")
+          {printf("b");}
+        else if (cmd=="C")
+          {printf("c");}
+        else if (cmd=="D")
+          {printf("d");}
+        else if (cmd=="E")
+          {printf("e");}
+        else
+            break;
+
+     }
+return0;
+}
